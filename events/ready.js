fa4d49e5c7863d891b59d55ae12bc87e95fdc60c
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-08 23:54:27 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
-const { Client } = require('discord.js');
-const config = require('../../config.json');
-
 module.exports = {
-    onReady: (client) => {
-        console.log(`Logged in as ${client.user.tag}!`);
-        client.user.setActivity(config.defaultStatus, { type: 'WATCHING' });
-    }
+  name: 'ready',
+  once: true,
+  execute(client) {
+    console.log(`Logged in as ${client.user.tag}`);
+    const config = require('../config.json');
+    client.user.setActivity(config.status);
+  }
 };
