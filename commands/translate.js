7209827439dbfce434025e925f238723954efb47
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-08 23:53:12 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,22 +1,27 @@
-const { translateText } = require('../utils/languageSupport');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
 
 module.exports = {
-    name: 'translate',
-    description: 'Translates text to a specified language.',
-    async execute(interaction) {
-        const textToTranslate = interaction.options.getString('text');
-        const targetLanguage = interaction.options.getString('language');
+  data: new SlashCommandBuilder()
+    .setName('translate')
+    .setDescription('Translate text to a specified language')
+    .addStringOption(option => option.setName('text').setDescription('Text to translate').setRequired(true))
+    .addStringOption(option => option.setName('language').setDescription('Target language').setRequired(true)),
+  async execute(interaction) {
+    const text = interaction.options.getString('text');
+    const language = interaction.options.getString('language');
 
-        if (!textToTranslate || !targetLanguage) {
-            return interaction.reply('Please provide both text and target language.');
+    try {
+      const response = await axios.get('https://api.translate.com/translate', {
+        params: {
+          text: text,
+          target_lang: language
         }
-
-        try {
-            const translatedText = await translateText(textToTranslate, targetLanguage);
-            return interaction.reply(translatedText);
-        } catch (error) {
-            console.error(error);
-            return interaction.reply('There was an error translating the text. Please try again later.');
-        }
-    },
+      });
+      await interaction.reply(response.data.translated_text);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply('Failed to translate text.');
+    }
+  }
 };
